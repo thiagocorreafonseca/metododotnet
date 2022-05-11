a1fe831f7cf24a9d86a87880fd01c2ee605f3c3b
Thiago Correa da Fonseca <thiagocorreafonseca@gmail.com>
2022-05-11 14:01:10 -0300

    tipos de dados e estruturas condicionais

---
+++ NetDiretoAoPonto/EstruturasCondicao/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace EstruturasCondicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //if else
+            var notaDigitada = Console.ReadLine();
+            var nota = int.Parse(notaDigitada);
+
+            if(nota >= 70)
+            {
+                Console.WriteLine("Aprovado");
+            }else if (nota >= 40)
+            {
+                Console.WriteLine("Em recuperação");
+            }else
+            {
+                Console.WriteLine("Reprovado");
+            }
+
+            //switch case
+            Console.WriteLine("Bem Vindo.");
+
+            Console.WriteLine("Digite 1 - Contratação de Serviço");
+            Console.WriteLine("Digite 2 - Reclamação de Serviço");
+            Console.WriteLine("Digite 3 - Segunda via de Boleto");
+            Console.WriteLine("Digite 4 - Sair");
+
+            var opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    Console.WriteLine("Informações sobre serviço.");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Fale sobre sua reclamação.");
+                    break;
+
+                case "3":
+                    Console.WriteLine("Segunda via do boleto enviada por e-mail.");
+                    break;
+
+                case "4":
+                    Console.WriteLine("Tenha um bom dia.");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+            }
+        }
+    }
+}
---
+++ NetDiretoAoPonto/TiposDadosConversoesOperadores/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace TiposDadosConversoesOperadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Tipos de Dados
+
+            int numeroInt = 10;
+            int maiorNumeroInt = int.MaxValue;
+            int menorNumeroInt = int.MinValue;
+
+            long numeroLong = 1231231231231123123;
+            long maiorNumeroLong = long.MaxValue;
+            long menorNumeroLong = long.MinValue;
+
+            decimal numeroDecimal = 10.52m;
+
+            double numeroDouble = 12.3;
+            double menorNumeroDouble = double.MinValue;
+            double maiorNumeroDouble = double.MaxValue;
+
+            bool verdadeiro = true;
+            bool falso = false;
+
+            var numero = 10;
+
+            string nome = "Nome Bla Bla Bla";
+            char letra = 'T';
+
+            DateTime entradaEmpresa = new DateTime(2022, 1, 1);
+            TimeSpan quantoTempoDeEmpresa = DateTime.Now - entradaEmpresa;
+            #endregion
+
+            #region Conversões
+            int notaAluno = 10;
+
+            //implicita
+            double notaAlunoDouble = notaAluno;
+
+            //explicita
+            int numeroDoubleComoInt = (int)notaAlunoDouble;
+
+            //utilizando Convert
+            string notaString = "10";
+
+            int notaConvert = Convert.ToInt32(notaString);
+
+            //utilizando Parse
+            int notaParse = int.Parse(notaString);
+
+            if(int.TryParse(notaString, out int notaTryParse))
+            {
+
+            }else
+            {
+                Console.WriteLine("Número em formato inválido.");
+            }
+            #endregion
+
+            #region Operadores Aritmeticos
+            //Unários ++ -- + -
+            int numeroOperador = 4;
+
+            Console.WriteLine(numeroOperador++);
+            Console.WriteLine(numeroOperador--);
+
+            Console.WriteLine(++numeroOperador);
+            Console.WriteLine(--numeroOperador);
+
+            Console.WriteLine(numeroOperador);
+            Console.WriteLine(-numeroOperador);
+            Console.WriteLine(-(-numeroOperador));
+
+            //Binários + - * /
+            var soma = 4 + 5;
+            var subtracao = 4 - 5;
+            var multiplicacao = 4 * 5;
+            var divisao = 20 / 3;
+            var divisaoDouble = (double)20 / 3;
+
+            var multiplos = (4 * 5) + 10;
+            #endregion
+
+            #region Operadores Comparação
+            //> >= < <=
+            Console.WriteLine(4 > 5);
+            Console.WriteLine(5 > 5);
+            Console.WriteLine(5 >= 6);
+
+            Console.WriteLine(5 < 4);
+            Console.WriteLine(5 <= 5);
+            Console.WriteLine(5 < 6);
+            #endregion
+
+            #region Operadores de Igualdade
+            // == !=
+            Console.WriteLine(5 == 5);
+            Console.WriteLine(5 == 4);
+
+            Console.WriteLine(5 != 5);
+            Console.WriteLine(4 != 5);
+            #endregion
+
+            #region Operadores Lógicos
+            // AND(&&) OR(||)
+            var minhaNota = 7;
+            var ultimoAno = true;
+
+            if (minhaNota >= 7 && ultimoAno)
+            {
+                Console.WriteLine("Aprovado e terminou o curso.");
+            }
+
+            minhaNota = 4;
+            if(minhaNota >= 7 || ultimoAno)
+            {
+                Console.WriteLine("Aprovado");
+            }
+
+            Console.WriteLine(true || false);
+            Console.WriteLine(false || true);
+            Console.WriteLine(true || true);
+            Console.WriteLine(false || false);
+
+            Console.WriteLine(true && false);
+            Console.WriteLine(false && true);
+            Console.WriteLine(true && true);
+            Console.WriteLine(false && false);
+            #endregion
+        }
+
+    }
+}
