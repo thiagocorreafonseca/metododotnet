ba56bb48281765f6169e28106b0d7f1c4d659c7e
Thiago Correa da Fonseca <thiagocorreafonseca@gmail.com>
2022-05-20 20:40:08 -0300

    Exceções

---
+++ NetDiretoAoPonto/DEbugandoAplicacoes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DEbugandoAplicacoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Debugging
+            var numerosString = Console.ReadLine();
+            var numeros = numerosString.Split(' ');
+
+            foreach (var numero in numeros)
+            {
+                var numeroInt = int.Parse(numero);
+                var aoQuadrado = Math.Pow(numeroInt, 2);
+
+                Console.WriteLine($"Número: {numeroInt}² - {aoQuadrado}");
+            }
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NetDiretoAoPonto/TratandoExcecoes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TratandoExcecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Tratando Exceções
+            var seteString = "sete";
+            string valorNull = null;
+            var longValue = long.MaxValue.ToString();
+
+            try
+            {
+                //var formatException = int.Parse(seteString);
+                //var argumentNullException = int.Parse(valorNull);
+                var overflowException = int.Parse(longValue);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"FormatException : {ex.Message}");           
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Argument Null Exception : {ex.Message}");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine($"Overflow Exception : {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine($"Try-Finally executado.");
+            }
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
